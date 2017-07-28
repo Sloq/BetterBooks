b0bc9daf057abe62c09b82af9f7e68e06a25fbd5
Stephen Loquet <sloquet@gmail.com>
2017-07-28 15:36:21 -0700

    remove stuff from window

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,15 +3,8 @@ import ReactDom from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { receiveAllBooks, requestAllBooks } from './actions/book_actions';
-
-
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.requestAllBooks = requestAllBooks;
-  window.receiveAllBooks = receiveAllBooks;
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser, errors: [] } };
     store = configureStore(preloadedState);
