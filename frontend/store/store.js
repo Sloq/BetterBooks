413aaa2f330f45e21f15f42aea2a805447729431
Stephen Loquet <sloquet@gmail.com>
2017-07-28 15:32:43 -0700

    fix redux logger bug

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
 import RootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
   // must use 'require' (import only allowed at top of file)
-  middlewares.push(logger);
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger.logger);
+  console.log(middlewares);
 }
 
 const configureStore = (preloadedState = {}) => (
