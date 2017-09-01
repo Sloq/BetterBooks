2a6b848e54166dece2e0a5a1873e10dfac14c9f3
Stephen Loquet <sloquet@gmail.com>
2017-09-01 13:58:07 -0700

    change webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,49 @@
 const path = require("path");
+const webpack = require("webpack");
 
-var webpack = require("webpack");
+let plugins = []; // if using any plugins for both dev and production
+let devPlugins = []; // if using any plugins for development
 
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
 
-// var prodPlugins = [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }),
-//   new webpack.optimize.UglifyJsPlugin({
-//     compress: {
-//       warnings: true
-//     }
-//   })
-// ];
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
-new webpack.DefinePlugin({
-  'process.env': {
-    NODE_ENV: JSON.stringify('production')
-  }
-}),
-new webpack.optimize.UglifyJsPlugin()
-
-// plugins = plugins.concat(
-//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// );
-
+// include plugins config
 module.exports = {
   context: __dirname,
   entry: "./frontend/index.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"]
-  },
-  devtool: 'source-maps',
+  plugins: plugins,
   module: {
     loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
-    },
-    {
-      test: /\.node$/,
-      loader: "node-loader"
-    }
     ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
   }
 };
